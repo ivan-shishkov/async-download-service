37c38c211ed495b5945a87034d64c757c81cf53b
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-04-28 13:41:04 +0300

    Added getting of parameters of working through either command line arguments or environment variables

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 import asyncio
 import os.path
 import logging
+import os
+import argparse
 
 from aiohttp import web
 import aiofiles
 
+enable_logging = False
+enable_response_delay = False
+storage_base_path = 'test_photos'
+
 
 async def archivate(request):
-    store_base_path = 'test_photos'
-
     archive_hash = request.match_info['archive_hash']
 
-    archive_content_path = os.path.join(store_base_path, archive_hash)
+    archive_content_path = os.path.join(storage_base_path, archive_hash)
 
     if not os.path.exists(archive_content_path):
         raise web.HTTPNotFound(text='Archive does not exists or was removed')
@@ -37,6 +41,9 @@ async def archivate(request):
             if not archive_chunk:
                 break
 
+            if enable_response_delay:
+                await asyncio.sleep(1)
+
             logging.info(
                 f'Sending archive chunk #{chunk_number} '
                 f'with size {len(archive_chunk)} bytes ...'
@@ -44,8 +51,6 @@ async def archivate(request):
             await response.write(archive_chunk)
 
             chunk_number += 1
-
-            await asyncio.sleep(1)
 
     except asyncio.CancelledError:
         logging.info('Cancelled error')
@@ -66,8 +71,51 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
+def get_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-l',
+        '--logging',
+        help='Enables logging',
+        action='store_true',
+    )
+    parser.add_argument(
+        '-d',
+        '--delay',
+        help='Enables delay for send response',
+        action='store_true',
+    )
+    parser.add_argument(
+        '-p',
+        '--path',
+        help='Base path for store',
+        type=str,
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    command_line_arguments = get_command_line_arguments()
+
+    enable_logging = bool(
+        command_line_arguments.logging or
+        os.getenv('ENABLE_LOGGING') or
+        enable_logging
+    )
+    enable_response_delay = bool(
+        command_line_arguments.delay or
+        os.getenv('ENABLE_RESPONSE_DELAY') or
+        enable_response_delay
+    )
+    storage_base_path = (
+            command_line_arguments.path or
+            os.getenv('STORE_BASE_PATH') or
+            storage_base_path
+    )
+
+    if enable_logging:
+        logging.basicConfig(level=logging.INFO)
 
     app = web.Application()
     app.add_routes([
